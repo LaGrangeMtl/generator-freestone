d42efdbdd80afc2d5005b754f5d9adeddf6bc694
Nicolas Poirier-Barolet <picolo@la-grange.ca>
2017-01-11 15:45:33 -0500

    Small fixes in deploy.sh

--- app/templates/_deploy.sh
+++ app/templates/_deploy.sh
@@ -14,9 +14,9 @@ cd ../..
 setPermissions
 
 # deploy uploads - should be runned only once.
-# rsync -avzLR --progress --no-p --groupmap=*:webmasters ./c/<%= props.projectNamespace %/uploads ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
+# rsync -avzLR --progress --no-p --groupmap=*:webmasters ./c/<%= props.projectNamespace %>/uploads ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 # create the _cache folder, only once too
-# ssh ubuntu@enclos.ca "sudo mkdir /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %/_cache && sudo chmod -R 755 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %/_cache"
+# ssh ubuntu@enclos.ca "sudo mkdir /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/_cache && sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/_cache"
 
 rsync -avzL --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude . ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 
