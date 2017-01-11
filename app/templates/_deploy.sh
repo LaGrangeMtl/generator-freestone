26a39bbf11b836933327c37febe140b070a3f077
Martin Vézina <m.vezina@la-grange.ca>
2017-01-11 09:56:49 -0500

    deploy pas old freestone

--- app/templates/_deploy.sh
+++ app/templates/_deploy.sh
@@ -5,9 +5,8 @@ setPermissions() {
 		sudo chown -R ubuntu:webmasters /var/www/<%= props.projectNamespace %>.enclos.ca/public_html &&
 		sudo chmod -R 755 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html &&
 		sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/admin/thumbnails &&
-		sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/file_db
 		sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/lang
-		sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/img_db";
+		sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/uploads";
 }
 
 cd ../..
@@ -17,8 +16,6 @@ setPermissions
 rsync -avzL --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude . ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 
 rsync -avzL --progress --no-p --groupmap=*:webmasters ./c/<%= props.projectNamespace %>/config/. ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html
-
-rsync -avzLR --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude ./c/_default ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 
 rsync -avzLR --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude ./c/<%= props.projectNamespace %> ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 
