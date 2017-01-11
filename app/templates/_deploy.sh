f458a86eb5c5efcc8b6c2931520841600bbb9ac0
Maxime Des Roches <m.desroches@la-grange.ca>
2017-01-11 14:04:27 -0500

    added _cache and uploads to deploy

--- app/templates/_deploy.sh
+++ app/templates/_deploy.sh
@@ -13,6 +13,11 @@ cd ../..
 
 setPermissions
 
+# deploy uploads - should be runned only once.
+# rsync -avzLR --progress --no-p --groupmap=*:webmasters ./c/<%= props.projectNamespace %/uploads ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
+# create the _cache folder, only once too
+# ssh ubuntu@enclos.ca "sudo mkdir /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %/_cache && sudo chmod -R 755 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %/_cache"
+
 rsync -avzL --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude . ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 
 rsync -avzL --progress --no-p --groupmap=*:webmasters ./c/<%= props.projectNamespace %>/config/. ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html
