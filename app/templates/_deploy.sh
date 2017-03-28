304e12cb1c9dcb0278158fea9fade0d1708d97f6
Martin Vézina <m.vezina@la-grange.ca>
2017-03-28 15:02:04 -0400

    exclude prc pull

--- app/templates/_deploy.sh
+++ app/templates/_deploy.sh
@@ -21,7 +21,7 @@ setPermissions
 	#prend les fichiers de lang et les imgdb, etc. (doit être actif si le client peut changer les langs)
 	# rsync -avzL --progress ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/admin/thumbnails ./admin/
 	# rsync -avzL --progress ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/lang .
-	# rsync -avzL --progress ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/uploads .
+	# rsync -avzL --progress --exclude 'prc*' ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/uploads .
 
 rsync -avzL --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude . ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 
