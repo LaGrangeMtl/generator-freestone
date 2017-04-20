183197e850591a0d4ecd0eac0a58311acecce53a
Anabelle Rajotte <a.rajotte@la-grange.ca>
2017-04-20 13:35:14 -0400

    add edge fix

--- app/templates/src/app/Main.js
+++ app/templates/src/app/Main.js
@@ -6,4 +6,14 @@ import { docReady } from './utils/docReady';
 
 docReady.then(() => {
 	
+	// Edge srcset fix
+	function isEdge(versions) { // array of numbers
+		return versions.reduce((c, version) => {
+			return c + (~window.navigator.userAgent.indexOf(`Edge/${version}`) ? 1 : 0);
+		}, 0) !== 0;
+	}
+
+	if (isEdge([12, 13, 14])) {
+		$('img').removeAttr('srcset').removeAttr('sizes');
+	}
 });
