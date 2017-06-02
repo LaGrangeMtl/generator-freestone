2cc9ebe655731b74ecebef817fdac509435ff128
Nicolas Poirier-Barolet <picolo@la-grange.ca>
2017-06-02 09:41:22 -0400

    Modifs utils

--- app/templates/src/app/Main.js
+++ app/templates/src/app/Main.js
@@ -3,16 +3,9 @@
 
 import $ from 'jquery';
 import { docReady } from './utils/docReady';
+import { isEdge } from './utils/browserDetect'; 
 
 docReady.then(() => {
-	
-	// Edge srcset fix
-	function isEdge(versions) { // array of numbers
-		return versions.reduce((c, version) => {
-			return c + (~window.navigator.userAgent.indexOf(`Edge/${version}`) ? 1 : 0);
-		}, 0) !== 0;
-	}
-
 	if (isEdge([12, 13, 14])) {
 		$('img').removeAttr('srcset').removeAttr('sizes');
 	}
--- app/templates/src/app/utils/scrollTo.js
+++ app/templates/src/app/utils/scrollTo.js
@@ -1,34 +1,49 @@
 import gsap, { TweenMax } from 'gsap';
 import $ from 'jquery';
 
+const $window = $(window);
+const defaults = {
+	offset: 0,
+	time: 1.2,
+	container: $window,
+	onUpdate() { return; },
+	onComplete() { return; },
+};
+
 /**
  * Scrolls to position using TweenMax. If an offset is 
  * defined, scrolls that amount higher than the target element.
  *
  * @param {Number} pos
- * @param {Number} offset
+ * @param {Object} options
  */
-export const scrollTo = (pos, offset = 0, time = 1.2) => {
-    const scroll = {
-        y: $window.scrollTop(),
-    };
+export const scrollTo = (pos, options = {}) => {
+	const opt = Object.assign({}, defaults, options);
 
-    TweenMax.to(scroll, time, {
-        y: pos + offset,
-        ease: Cubic.easeInOut,
-        onUpdate: () => {
-            $window.scrollTop(scroll.y);
-        },
-    });
+	const scroll = {
+		y: opt.container.scrollTop(),
+	};
+
+	TweenMax.to(scroll, opt.time, {
+		y: pos + opt.offset,
+		ease: gsap.Cubic.easeInOut,
+		onUpdate: () => {
+			opt.container.scrollTop(scroll.y);
+			opt.onUpdate(scroll.y);
+		},
+		onComplete: opt.onComplete,
+	});
 };
 
 /**
  * Scrolls to an element.
  *
- * @param {jQuery Object | DOM Node} el
- * @param {Number} offset
+ * @param {JQuery} el
+ * @param {Object} options
  */
-export const scrollToElem = (el, offset = 0) => {
-    const $el = $(el);
-    scrollTo($el.offset().top, offset);
+export const scrollToElem = (el, options = {}) => {
+	const opt = Object.assign({}, defaults, options);
+
+	const $el = $(el);
+	scrollTo($el.offset().top, opt);
 };
