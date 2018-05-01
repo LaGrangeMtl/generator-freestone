ad300ae4a72e2c655a3cf963f012192017f6cc9e
Anabelle Rajotte <a.rajotte@la-grange.ca>
2018-05-01 16:48:50 -0400

    deploy

--- app/templates/_deploy.sh
+++ app/templates/_deploy.sh
@@ -9,14 +9,15 @@ setPermissions() {
 		sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/uploads";
 }
 
-cd ../..
-
-setPermissions
-
 # deploy uploads - should be runned only once.
 # rsync -avzLR --progress --no-p --groupmap=*:webmasters ./c/<%= props.projectNamespace %>/uploads ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/
 # create the _cache folder, only once too
 # ssh ubuntu@enclos.ca "sudo mkdir /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/_cache && sudo chmod -R 775 /var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/_cache"
+
+cd ../..
+
+setPermissions
+
 	
 	#prend les fichiers de lang et les imgdb, etc. (doit être actif si le client peut changer les langs)
 	# rsync -avzL --progress ubuntu@enclos.ca:/var/www/<%= props.projectNamespace %>.enclos.ca/public_html/c/<%= props.projectNamespace %>/admin/thumbnails ./admin/
