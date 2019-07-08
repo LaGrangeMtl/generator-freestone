08f9780f70c0ce21b39d276ea7949bed53b016d3
Maxime Des Roches <m.desroches@la-grange.ca>
2019-07-08 14:11:48 -0400

    ftp procedure wip

---
+++ generators/app/templates/scripts/configs/config-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+is_ftp='true'
+
+ftp_hostname=192.168.1.199
+ftp_localdir=~/ftp-temp
+ftp_user=''
+ftp_password=''
+ftp_remotedir='/'
--- generators/app/templates/scripts/deploy-enclos.sh
+++ generators/app/templates/scripts/deploy-enclos.sh
@@ -1,38 +1,71 @@
 #!/bin/bash
 
-NAMESPACE=<%= props.projectNamespace %>
+NAMESPACE=test #<%= props.projectNamespace %>
+install='false'
+pull_images='false'
+env='dev'
+
+LOG='\033[0;36m'
+NC='\033[0m'
+
+echo "./configs/config-${env}.sh"
+. ./configs/config-${env}.sh
+
+echo ${is_ftp}
+
+while getopts 'ipc:' flag; do
+  case "${flag}" in
+    i) install='true' ;;
+    p) pull_images='true' ;;
+    c) env=$OPTARG ;;
+  esac
+done
+
+BASE_PATH="/var/www/${NAMESPACE}.enclos.ca/public_html"
+
+if [ "${is_ftp}" == 'true' ]; then
+	curlftpfs ${ftp_hostname}/${ftp_remotedir} ${ftp_localdir} -o user=${ftp_user}:${ftp_password}
+	exit;
+fi
+exit;
 
 setPermissions() {
 	ssh ubuntu@enclos.ca "
-		sudo chown -R ubuntu:webmasters /var/www/${NAMESPACE}.enclos.ca/public_html &&
-		sudo chmod -R 755 /var/www/${NAMESPACE}.enclos.ca/public_html &&
-		sudo chmod -R 775 /var/www/${NAMESPACE}.enclos.ca/public_html/c/${NAMESPACE}/admin/thumbnails &&
-		sudo chmod -R 775 /var/www/${NAMESPACE}.enclos.ca/public_html/c/${NAMESPACE}/uploads";
+		sudo chown -R ubuntu:webmasters ${BASE_PATH} &&
+		sudo chmod -R 755 ${BASE_PATH} &&
+		sudo chmod -R 775 ${BASE_PATH}/_cache &&
+		sudo chmod -R 775 ${BASE_PATH}/admin/thumbnails &&
+		sudo chmod -R 775 ${BASE_PATH}/uploads";
 }
 
-# deploy uploads - should be runned only once.
-# rsync -avzLR --progress --no-p --groupmap=*:webmasters ./c/${NAMESPACE}/uploads ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html/
-# create the _cache folder, only once too
-# ssh ubuntu@enclos.ca "sudo mkdir /var/www/${NAMESPACE}.enclos.ca/public_html/c/${NAMESPACE}/_cache && sudo chmod -R 775 /var/www/${NAMESPACE}.enclos.ca/public_html/c/${NAMESPACE}/_cache"
+cd ..
 
-	#prend les fichiers de lang et les imgdb, etc. (doit être actif si le client peut changer les langs)
-	# rsync -avzL --progress ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html/c/${NAMESPACE}/admin/thumbnails ./admin/
-	# rsync -avzL --progress ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html/c/${NAMESPACE}/lang .
-	# rsync -avzL --progress --exclude 'prc*' ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html/c/${NAMESPACE}/uploads .
+setPermissions > /dev/null 2>&1
 
-cd ../..
+if [ ${install} == 'true' ]; then
+	echo "${LOG}Installing...${NC}";
+	# deploy uploads - should be runned only once.
+	rsync -avzL --progress --no-p --groupmap=*:webmasters dist/uploads/ ubuntu@enclos.ca:${BASE_PATH}/uploads/
+	
+	# create the _cache folder, only once too
+	ssh ubuntu@enclos.ca "sudo mkdir -p ${BASE_PATH}/_cache && sudo chmod -R 775 ${BASE_PATH}/_cache"
+fi
 
-setPermissions
+if [ ${pull_images} == 'true' ]; then
+	echo "${LOG}Pulling images...${NC}";
+	# pull les images du serveur
+	rsync -avzL --progress ubuntu@enclos.ca:${BASE_PATH}/admin/thumbnails ./admin/
+	rsync -avzL --progress --exclude 'prc*' ubuntu@enclos.ca:${BASE_PATH}/uploads/ ./dist/uploads/
+fi
 
-rsync -avzL --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude . ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html/
+echo "${LOG}Synching dist files...${NC}";
+rsync -avzL --progress --delete --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude dist/ ubuntu@enclos.ca:${BASE_PATH}/
 
-rsync -avzL --progress --no-p --groupmap=*:webmasters ./c/${NAMESPACE}/config/. ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html
+if [ "${env}" != '' ]; then
+	echo "${LOG}Synching config files...${NC}";
+	rsync -avzL --progress --no-p --groupmap=*:webmasters config/EnvConfig-${env}.php ubuntu@enclos.ca:${BASE_PATH}/config/EnvConfig.php
+fi
 
-rsync -avzLR --progress --no-p --groupmap=*:webmasters --exclude-from=.rsync.exclude ./c/${NAMESPACE} ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html/
+setPermissions > /dev/null 2>&1
 
-ssh ubuntu@enclos.ca mkdir -p /var/www/${NAMESPACE}.enclos.ca/public_html/admin
-rsync -avzL --progress --no-p --groupmap=*:webmasters admin/dist/. ubuntu@enclos.ca:/var/www/${NAMESPACE}.enclos.ca/public_html/admin
-
-setPermissions
-
-cd c/${NAMESPACE}
+cd scripts
