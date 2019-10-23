9d244ce9d164e850c30b7956f99f82358215ae17
Alexandre Peloquin <apeloquin@aliceandsmith.com>
2019-10-23 09:26:17 -0400

    Fixed bootstrapping for SendFeedback

--- generators/app/templates/dist/modules/SendFeedback.php
+++ generators/app/templates/dist/modules/SendFeedback.php
@@ -1,34 +1,32 @@
-<?php	
-	$root = __DIR__ . '/../../..';
-	require_once($root.'/freestone/bootstrapFreestone.php');
-	bootstrapFreestone($root);
-	
-	use Freestone\Frontendv2\Modules\Feedback\Email;
-	use Freestone\Frontendv2\Modules\Feedback\FeedbackConstants;
+<?php
 
-	if (isset($_POST['feedback'])) {
-		header('Content-Type: application/json');
-		$sentMessages = [];
-		
-		foreach ($_POST['feedback'] as $id) {
-			$email = new Email($id);
-			$res = (object) $email->send();
+require_once('../bootstrap.php');
 
-			if ($res->code !== FeedbackConstants::CODE_OK) {
-				http_response_code($res->code);
-				$res->sentMessages = $sentMessages;
+use Freestone\Frontendv2\Modules\Feedback\Email;
+use Freestone\Frontendv2\Modules\Feedback\FeedbackConstants;
 
-				echo json_encode($res);
-				die();
-			} else {
-				$sentMessages[] = $id;
-			}
+if (isset($_POST['feedback'])) {
+	header('Content-Type: application/json');
+	$sentMessages = [];
+
+	foreach ($_POST['feedback'] as $id) {
+		$email = new Email($id);
+		$res = (object) $email->send();
+
+		if ($res->code !== FeedbackConstants::CODE_OK) {
+			http_response_code($res->code);
+			$res->sentMessages = $sentMessages;
+
+			echo json_encode($res);
+			die();
+		} else {
+			$sentMessages[] = $id;
 		}
-
-		echo json_encode((object) [
-			'code' => FeedbackConstants::CODE_OK,
-			'messages' => [],
-			'sentMessages' => $sentMessages,
-		]);
 	}
-?>
+
+	echo json_encode((object) [
+		'code' => FeedbackConstants::CODE_OK,
+		'messages' => [],
+		'sentMessages' => $sentMessages,
+	]);
+}
