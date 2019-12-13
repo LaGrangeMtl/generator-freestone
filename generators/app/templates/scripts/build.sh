659700b45a2587b28a8cbd253bc3beb49a78522d
Étienne <e.ouellet@la-grange.ca>
2019-12-13 09:45:17 -0500

    update build.sh pour la nouvelle structure

--- generators/app/templates/scripts/build.sh
+++ generators/app/templates/scripts/build.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
+npm install
 cd dist
 composer update
 cd ..
-npm install
 rm -rf dist/assets/js
 rm -rf dist/assets/css
-parcel build 'src/+(css|js)/index.+(js|scss)' -d dist/assets --no-source-maps
+parcel build 'src/(css|js)/index.(scss|js)' --public-url /assets -d dist/assets --no-source-maps
