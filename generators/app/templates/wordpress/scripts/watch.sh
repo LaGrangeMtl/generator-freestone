609626a283c7a2c86d74c86406ec96b4f9b8cd00
Alexandre Peloquin <a.peloquin@la-grange.ca>
2021-03-18 21:16:14 -0400

    Adapted for parcel v2.

--- generators/app/templates/wordpress/scripts/watch.sh
+++ generators/app/templates/wordpress/scripts/watch.sh
@@ -1,2 +1,2 @@
-parcel watch 'src/(css|js)/(index|index-admin).(scss|js)' --public-url /wp-content/themes/<%= props.projectNamespace %>/assets -d ./dist/wp-content/themes/<%= props.projectNamespace %>/assets --no-cache --no-hmr &
+parcel watch 'src/(css|js)/(index|index-admin).(scss|js)' --public-url /wp-content/themes/<%= props.projectNamespace %>/assets --dist-dir ./dist/wp-content/themes/<%= props.projectNamespace %>/assets --no-cache --no-hmr &
 livereload ./dist/wp-content/themes/<%= props.projectNamespace %>/assets
