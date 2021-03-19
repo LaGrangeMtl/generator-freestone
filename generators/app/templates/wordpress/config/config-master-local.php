75ed3340ad062ffab83c64630e1847309dd24703
Alexandre Peloquin <a.peloquin@la-grange.ca>
2021-03-18 21:15:05 -0400

    deleted config-master-local.php since we will use dev instead.

--- generators/app/templates/wordpress/config/config-master-local.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define( 'DB_HOST', 'localhost' );
-define( 'DB_NAME', '<%= props.projectName %>_master_local' );
-define( 'DB_USER', '<%= props.projectName %>_master_local' );
-define( 'DB_PASSWORD', '<%= props.dbPassLocal %>' );
-
-define( 'WP_DEBUG', true );
