bc907d8f75ef37e0625e7f2cfdc0596639e37e00
Alexandre Peloquin <a.peloquin@la-grange.ca>
2021-03-18 20:57:36 -0400

    Removed composer install from build.sh

--- generators/app/templates/common/scripts/build.sh
+++ generators/app/templates/common/scripts/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 npm install
-cd dist
-composer install
-cd ..
 rm -rf dist/assets/js
 rm -rf dist/assets/css
-parcel build 'src/(css|js)/index*(*).(scss|js)' --public-url /assets -d dist/assets --no-source-maps
+parcel build 'src/(css|js)/index*(*).(scss|js)' --public-url /assets --dist-dir dist/assets --no-source-maps --no-scope-hoist
