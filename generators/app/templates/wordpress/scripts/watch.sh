66fbd66c10e4c7de46b9c5a61cdfa2c9d0530a8e
Maxime Des Roches <m.desroches@la-grange.ca>
2021-05-25 11:39:50 -0400

    added wordpress conf

--- generators/app/templates/wordpress/scripts/watch.sh
+++ generators/app/templates/wordpress/scripts/watch.sh
@@ -1,3 +1,2 @@
 echo "<?php define('ASSETS_VERSION', '`date '+%Y%m%d%H%M%S'`');" > ./dist/assets-version.php
-parcel watch 'src/(css|js)/(index|index-admin).(scss|js)' --public-url /wp-content/themes/<%= props.projectNamespace %>/assets --dist-dir ./dist/wp-content/themes/<%= props.projectNamespace %>/assets --no-cache --no-hmr &
-livereload ./dist/wp-content/themes/<%= props.projectNamespace %>/assets
+parcel serve 'src/(css|js)/index*(*).(scss|ts|js)' --public-url /assets --dist-dir ./dist/wp-content/themes/<%= props.projectNamespace %>/assets --no-cache --target main --cert ./.ssl/fullchain.pem --key ./.ssl/privkey.pem
