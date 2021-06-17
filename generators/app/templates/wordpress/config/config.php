7cd28ffaa7946c3c5196dab59051446fbbbbf231
Alexandre Peloquin <a.peloquin@la-grange.ca>
2021-06-17 11:01:16 -0400

    Added EnvConfig.php and assets-version.php to .gitignore (Same logic for WordPress)

---
+++ generators/app/templates/wordpress/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define( 'DB_HOST', 'dev.enclos.ca' );
+define( 'DB_NAME', '<%= props.projectName %>_master_dev' );
+define( 'DB_USER', '<%= props.projectName %>_master_dev' );
+define( 'DB_PASSWORD', '<%= props.dbPassDev %>' );
+
+define( 'WP_DEBUG', true );
