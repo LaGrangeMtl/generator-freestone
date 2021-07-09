bd4ff1d02b75ed3f1d78d8149606ac4ae73a8f29
Alexandre Peloquin <a.peloquin@la-grange.ca>
2021-07-09 15:33:02 -0400

    Replaced generator variable by env variable

--- generators/app/templates/wordpress/scripts/build.sh
+++ generators/app/templates/wordpress/scripts/build.sh
@@ -1,5 +1,9 @@
 echo "<?php define('ASSETS_VERSION', '`date '+%Y%m%d%H%M%S'`');" > ./dist/assets-version.php
-rm -rf /dist/wp-content/themes/<%= props.projectNamespace %>/assets/js
-rm -rf /dist/wp-content/themes/<%= props.projectNamespace %>/assets/css
+
+project=${PWD##*/}
+project=${project,,}
+
+rm -rf /dist/wp-content/themes/${project}/assets/js
+rm -rf /dist/wp-content/themes/${project}/assets/css
 npm install
-parcel build 'src/+(css|js)/(index|index-admin).+(js|scss)' --public-url /wp-content/themes/<%= props.projectNamespace %>/assets --dist-dir ./dist/wp-content/themes/<%= props.projectNamespace %>/assets --no-source-maps --no-scope-hoist
+parcel build 'src/+(css|js)/(index|index-admin).+(js|scss)' --public-url /wp-content/themes/${project}/assets --dist-dir ./dist/wp-content/themes/${project}/assets --no-source-maps --no-scope-hoist
--- generators/app/templates/wordpress/scripts/watch.sh
+++ generators/app/templates/wordpress/scripts/watch.sh
@@ -1,2 +1,4 @@
 echo "<?php define('ASSETS_VERSION', '`date '+%Y%m%d%H%M%S'`');" > ./dist/assets-version.php
-parcel serve 'src/(css|js)/index*(*).(scss|ts|js)' --public-url /assets --dist-dir ./dist/wp-content/themes/<%= props.projectNamespace %>/assets --no-cache --target main --cert ./.ssl/fullchain.pem --key ./.ssl/privkey.pem
+project=${PWD##*/}
+project=${project,,}
+parcel serve 'src/(css|js)/index*(*).(scss|ts|js)' --public-url /assets --dist-dir ./dist/wp-content/themes/${project}/assets --no-cache --target main --cert ./.ssl/fullchain.pem --key ./.ssl/privkey.pem
